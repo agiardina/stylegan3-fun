ca9833d31ae56f061044666f56c4c6d60ed98260
Andrea Giardina <git@andreagiardina.com>
2023-02-09 18:34:55 +0100

    Generate gray images

--- gen_images.py
+++ gen_images.py
@@ -134,7 +134,8 @@ def generate_images(
 
         img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
         img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
-        PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/seed{seed:04d}.png')
+        img = np.squeeze(img, axis=3)
+        PIL.Image.fromarray(img[0].cpu().numpy(), 'L').save(f'{outdir}/seed{seed:04d}.png')
 
 
 #----------------------------------------------------------------------------
